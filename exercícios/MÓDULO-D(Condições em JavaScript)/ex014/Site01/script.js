8c8ae3a10d91cf7300fcfd3758eb450802d9c7d6
CarlosBurghiBatista <acburghi@gmail.com>
2026-04-06 18:59:31 -0300

    Est-06-04-26

--- exercícios/MÓDULO-D(Condições em JavaScript)/ex014/Site01/script.js
+++ exercícios/MÓDULO-D(Condições em JavaScript)/ex014/Site01/script.js
@@ -1,16 +1,22 @@
 function carregar(){
+
 var msg = document.getElementById('msg')
 var img = document.getElementById('imagem')
+
 var data = new Date()
 var hora = data.getHours()
+
 msg.innerHTML = `Agora são ${hora} horas.`
+
 if (hora >= 0 && hora < 12) { 
     img.src = "Imagens/01-MANHÃ.png"
     document.body.style.background = '#e2cd9f'
-  } else if (hora >= 12 && hora < 18) {
+  
+  } else if (hora >= 12 && hora <= 18) {
     img.src = "Imagens/02-TARDE.png"
     document.body.style.background = '#b9846f'
-} else {
+}
+ else {
     img.src = "Imagens/03-NOITE.png"
     document.body.style.background = '#515154'
 }
