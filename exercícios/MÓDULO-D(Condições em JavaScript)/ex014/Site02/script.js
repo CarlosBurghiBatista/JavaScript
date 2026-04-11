b8a41d47cb058d467e0e48571e78d6e8c2dfb353
CarlosBurghiBatista <acburghi@gmail.com>
2026-04-11 16:48:56 -0300

    Est-11-04-26

--- exercícios/MÓDULO-D(Condições em JavaScript)/ex014/Site02/script.js
+++ exercícios/MÓDULO-D(Condições em JavaScript)/ex014/Site02/script.js
@@ -1,25 +1,23 @@
 function verificar() {
-  var data = new Date()
-  var ano  = data.getFullYear()
+  var data = new Date() //coleta a data do sistema automaticamente.
+  var ano  = data.getFullYear() //Armazenada pelo ano completo. 
   
-  var fano = document.getElementById('txtano')
-  var res  = document.getElementById('res')
-  var limit = 1926
+  var fano = document.getElementById('txtano')//armazena o 'ano'digitado no input (number).
+  var res  = document.getElementById('res') //conecta a div de id 'res' à função.
+  var limit = 1926 //variável criada para limitar a idade gerada em 100 anos.
+  var fsex = document.getElementsByName('radsex') //conecta os inputs tipo 'radio' à função.
+  var idade = ano - fano.value //Armazena - Ano atual menos o ano de nascimento.
+  var gênero = ''//Armazena o sexo selecionado no 'input-radio' ou no 'Label'.
 
-  if (fano.value.length == 0 || fano.value.length == 3 || fano.value > ano || fano.value < limit) {
+  var img = document.createElement('img')//Cria variável 'img' direto no script.js.
+      img.setAttribute('id', 'foto') //Atribui o arquivo de imagem por id.
+
+  if (fano.value > ano || fano.value < limit) {
     window.alert('[ERRO] Verifique os dados e tente novamente !')
   } 
-  else {
-    var fsex = document.getElementsByName('radsex')
-    var idade = ano - fano.value
-    var gênero = ''
-    
-   // var img = document.createElement('img')
-      //  img.setAttribute('id', 'foto')
-    
-    if (fsex[0].checked) { 
-      gênero = 'Homem'
-    /*  if (idade >= 0 && idade < 10){
+  else { if (fsex[0].checked) { gênero = 'Homem'
+     
+      if (idade >= 0 && idade < 10){
          img.setAttribute('src' , 'foto-Menino.png')
     } else if (idade < 21 ){
          img.setAttribute('src' , 'foto-Rapaz.png')
@@ -27,11 +25,11 @@ function verificar() {
          img.setAttribute('src' , 'foto-Adulto.png')
     } else {
          img.setAttribute('src' , 'foto-Idoso.png')
-    }*/
+    }
     
 } else if ( fsex[1]) { 
       gênero = 'Mulher'
-     /* if (idade >= 0 && idade < 10){
+      if (idade >= 0 && idade < 10){
         img.setAttribute('src' , 'foto-Menina.png')
     } else if (idade < 21 ){
         img.setAttribute('src' , 'foto-Moça.png')
@@ -39,14 +37,14 @@ function verificar() {
         img.setAttribute('src' , 'foto-Adulta.png')
     } else {
         img.setAttribute('src' , 'foto-Idosa.png')
-    }*/
+    }
     
 }
       
-      res.style.textAlign = 'center'
-      res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
-     // res.appendChild(img)
-  }
+      
+      res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`//O texto mostrado muda conforme as variáveis
+      res.appendChild(img) //Acrescenta 'filho' na árvore DOM.  
+    }
 
 
   
---
+++ exercícios/MÓDULO-E(Repetições em JavaScript)/while.js
@@ -0,0 +1,36 @@
+var c = 0
+do {console.log(`Passo ${c}`) 
+c++     
+} while (c < 6);
+
+
+
+console.log('\n')
+
+
+
+var d = 0
+while (d < 6) { console.log(`Fase ${d}`) 
+ d++}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
