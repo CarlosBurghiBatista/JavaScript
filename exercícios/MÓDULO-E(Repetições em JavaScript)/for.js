b966dd89eb5c5c360065584aef1074aa3d2df5a9
CarlosBurghiBatista <acburghi@gmail.com>
2026-04-12 19:45:49 -0300

    Est-12-04-26

---
+++ exercícios/MÓDULO-E(Repetições em JavaScript)/for.js
@@ -0,0 +1,11 @@
+console.log('\n')
+
+for(var b=0; b <= 4; b++) {console.log(`Passo ${b}`)}
+
+console.log('\n\n')
+
+console.log('Vai começar !')
+for(var c = 0; c < 5; c++) {console.log(c)}
+console.log('Fim !')
+
+console.log('\n')
